67b734691fadd7ad9baed6191f12c4433cfdec8f
andersbor <anbo@zealand.dk>
2024-11-12 09:11:15 +0100

    Teacher model, repo, controller

---
+++ RESTforCD10/Models/Teacher.cs
@@ -0,0 +1,17 @@
+﻿namespace RESTforCD10.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Salary { get; set; }
+
+        public void ValidateSalary()
+        {
+            if (Salary < 0)
+            {
+                throw new ArgumentOutOfRangeException("Salary is too low");
+            }
+        }
+    }
+}
