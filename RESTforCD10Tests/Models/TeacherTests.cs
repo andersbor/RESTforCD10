90eba3c1c06713e592d7da9f46960b334cc6760b
andersbor <anbo@zealand.dk>
2024-11-12 09:23:30 +0100

    unit test

---
+++ RESTforCD10Tests/Models/TeacherTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RESTforCD10.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTforCD10.Models.Tests
+{
+    [TestClass()]
+    public class TeacherTests
+    {
+        [TestMethod()]
+        public void ValidateSalaryTest()
+        {
+            Teacher teacherZeroSalary = new() { Name = "Anders", Salary = 0 };
+            Teacher teacherNegativeSalary = new() { Name = "Bente", Salary = -1 };
+            Teacher teacherPositiveSalary = new() { Name = "Carl", Salary = 1 };
+
+            teacherPositiveSalary.ValidateSalary();
+            teacherZeroSalary.ValidateSalary();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => teacherNegativeSalary.ValidateSalary());
+        }
+    }
+}
